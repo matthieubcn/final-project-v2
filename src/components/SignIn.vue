08f09f1d26337b32ebbd3359fccd030757358ebc
Matthieu Thuillier <thuillier.matthieu@hotmail.fr>
2022-12-05 21:39:37 +0100

    first commit

---
+++ src/components/SignIn.vue
@@ -0,0 +1,30 @@
+<!-- COMPONENTE BOILERPLATE -->
+ 
+  <template>
+
+  <div class="container">
+    <h3 class="header-title">Log In to To :Do App</h3>
+    <p class="header-subtitle">Estamos en la ruta de login. Aquí deberíais crear un form con la lógica correspondiente para que este permita al usuario loguearse con su email y su contraseña. Miraros la lógica de SignUp si necesitáis inspiración :)</p>
+    <p class="sign-in">Dont have an account? <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link"/></p>
+
+  </div>
+  <div class="container">
+  <img src ="https://images.unsplash.com/photo-1664575262619-b28fef7a40a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=832&q=80">
+  </div>
+
+</template>
+
+<script setup>
+import PersonalRouter from "./PersonalRouter.vue";
+
+// Route Variables
+const route = "/auth/signup";
+const buttonText = "Sign Up";
+
+// Arrow function to Signin user to supaBase
+const signIn = async () => {
+  try {} catch (error) {}
+};
+</script>
+
+<style></style>
